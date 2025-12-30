f0f51dfbd8bc2fd2296708a326d3c614f513fdb5
muaaz0333 <muaazahmad001@gmail.com>
2025-12-30 20:57:34 +0500

    fixed the footer

--- components/shared/footer.tsx
+++ components/shared/footer.tsx
@@ -1,6 +1,6 @@
 export function Footer() {
   return (
-    <footer className="bg-[#303030] text-white py-6 sm:py-8 px-4 sm:px-6">
+    <footer className="bg-[#303030] text-white lg:py-4 py-6 sm:py-8 px-4 sm:px-6">
       <div className="container mx-auto text-center">
         <p className="text-sm text-white">© WarrantyDB.com 2025</p>
       </div>
